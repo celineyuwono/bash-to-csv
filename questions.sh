089dafaf245ef0a724fdc2fcaa9666bb51eae2bc
Celine Yuwono <celineyuwono@gmail.com>
2016-10-31 20:31:32 +0000

    Add file and test run: questions.sh

---
+++ questions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo What is your name?
+read NAME
+echo What is your age?
+read AGE
+echo What is your favorite movie?
+read MOVIE
+echo What is your favorite song?
+read SONG
+echo What is your favorite book?
+read BOOK
